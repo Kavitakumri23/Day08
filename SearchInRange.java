bbc4ae4b5e3089d424a37efc1de69bd8fb6f4f9b
Kavitakumri23 <kroy22519@gmail.com>
2025-08-17 22:04:40 +0530

    Add files via upload

---
+++ SearchInRange.java
@@ -0,0 +1,22 @@
+public class SearchInRange {
+    public static void main(String[] args) {
+        int [] arr = {18,12,-7,2,14,28,19,16,-11,45};
+        int target = 19;
+        int ans = LinearSearch(arr, target);
+        System.out.println(ans);
+    }
+
+    static int LinearSearch(int [] arr, int target){
+        if(arr.length == 0){
+            return -1;
+        }
+
+        for(int index=0; index<=arr.length; index++){
+            int element = arr[index];
+            if(element==target){
+                return index;
+            }
+        }
+        return -1;
+    }
+}
